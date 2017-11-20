fb2295f71c9c061fffa6bdce501494483cb54195
andrezeRogal <andre.torgal@gmail.com>
2017-11-20 16:04:38 +0100

    add mongoose model with ref to another model

---
+++ snippets/mongoose-models/home-with-owner.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const homeSchema = new Schema({
+  name: String,
+  owner: {
+    type: ObjectId,
+    ref: 'User'
+  }
+});
+
+const Home = mongoose.model('Home', homeSchema);
+
+module.exports = Home;
