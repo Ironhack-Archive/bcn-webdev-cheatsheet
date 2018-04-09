4c0fa902b1ddcaed942226356593ff8feee170d3
tawebbcn <ta-web-bcn@ironhack.com>
2018-04-09 18:16:10 +0200

    Update es6.js

--- m2/es6/es6.js
+++ m2/es6/es6.js
@@ -1,8 +1,8 @@
 // -- ARROW FUNCTIONS --
-const doSomething = ('someparam', (param1, param2) => {
+const doSomething = (param1, param2) => {
     ...
     return result;
-});
+};
 
 const arr = [1,2,3,4];
 arr = arr.map((item) => item + 1);
