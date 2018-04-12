b931fd61530a124d3ef7554a3f315d3f8ff4327e
tawebbcn <ta-web-bcn@ironhack.com>
2018-04-12 12:29:32 +0200

    Update with-promises.js

--- m2/mongoose-querys/with-promises.js
+++ m2/mongoose-querys/with-promises.js
@@ -7,7 +7,20 @@ router.get('/', (req, res, next) => {
     const data = {
       places: result;
     }
-    res.json(data);
+    res.render('place', data);
+  });
+  promise.catch((error) => {
+    next(error);
+  });
+});
+
+router.post('/place', (req, res, next) => {
+  const newPlace = {
+    name: req.body.name,
+  }
+  const promise = Place.insertOne(newPlace);
+  promise.then(() => {
+    res.redirect('/');
   });
   promise.catch((error) => {
     next(error);
@@ -22,9 +35,22 @@ router.get('/', (req, res, next) => {
       const data = {
         places: result;
       }
-      res.json(data);
+      res.render('place', data);
     });
     .catch((error) => {
       next(error);
     });
+});
+
+router.post('/place', (req, res, next) => {
+  const newPlace = {
+    name: req.body.name,
+  }
+  Place.insertOne(newPlace)
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch((error) => {
+    next(error);
+  });
 });
