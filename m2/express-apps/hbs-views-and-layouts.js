3811e01d0269832dc9ad7321c6e87c54c263a451
Andre Torgal <andre.torgal@gmail.com>
2018-04-19 09:57:06 +0200

    Update hbs-views-and-layouts.js

--- m2/express-apps/hbs-views-and-layouts.js
+++ m2/express-apps/hbs-views-and-layouts.js
@@ -4,5 +4,4 @@ const hbs = require('hbs');
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/views'));
-app.use(express.static(path.join(__dirname, 'public')));
 hbs.registerPartials(path.join(__dirname, '/views/partials'));
