b4c7c262ed027269165adf91c61827d56f9d535c
Andre Torgal <andre.torgal@gmail.com>
2018-04-19 10:02:38 +0200

    Update user-with-auto-timestamps.js

--- m2/mongoose-models/user-with-auto-timestamps.js
+++ m2/mongoose-models/user-with-auto-timestamps.js
@@ -1,16 +1,21 @@
+'use strict';
+
 const mongoose = require("mongoose");
 const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
-  password: String
-}, {
-  timestamps: {
-    createdAt: "created_at",
-    updatedAt: "updated_at"
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
   }
+}, {
+  timestamps: true
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
