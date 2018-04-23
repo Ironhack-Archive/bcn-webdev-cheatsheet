da51feae25f5ccc29b284cb9d56fcf2d8511253c
Andre Torgal <andre.torgal@gmail.com>
2018-04-23 13:05:42 +0200

    Create flash.js

---
+++ m2/express-apps/flash.js
@@ -0,0 +1,33 @@
+// npm install --save connect-flash
+
+// -- app.js
+
+const flash = require('connect-flash');
+
+// --
+
+app.use(flash());
+
+// -- route that sends the message
+
+app.post('/form', (req, res, next) => { 
+  req.flash('message-name', 'The message content');
+  return res.redirect('/form');
+});
+
+// -- route that reads message
+
+app.get('/form', (req, res, next) => { 
+  const data = {
+    messages: req.flash('message-name');
+  };
+  res.render('template-name', data);
+});
+
+// -- template-name.hbs
+
+{{#if messages.length}}
+<ul class="validation">
+  {{#each messages}}<li>{{this}}</li>{{/each}}
+</ul>
+{{/if}}
