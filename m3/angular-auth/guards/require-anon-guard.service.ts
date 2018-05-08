22b3e4994ba322a52420e7d052fa91cd1b4358a5
Andre Torgal <andre.torgal@gmail.com>
2018-05-08 12:51:07 +0200

    Create require-anon-guard.service.ts

---
+++ m3/angular-auth/guards/require-anon-guard.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
+
+@Injectable()
+export class RequireAnonGuardService implements CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  canActivate(): Promise<boolean> {
+    return this.authService.me()
+      .then((user) => {
+        if (!user) {
+          return true;
+        } else {
+          this.router.navigate(['/']);
+          return false;
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        return false;
+      });
+  }
+
+}
